ab1c4498b8b5f017d4a21a0f8f5f71e6e2d98686
Владислав <v.pyslar@nyamee.com>
2022-05-16 11:49:07 +0300

    edit project

---
+++ src/views/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{text}}
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "@vue/runtime-core"
+
+export default defineComponent({
+  setup() {
+    const text = "Главная";
+
+    return {
+      text
+    }
+  },
+})
+</script>
